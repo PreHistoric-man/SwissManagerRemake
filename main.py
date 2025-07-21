dcb33808b50f990f95abf3cc2b35c80ece0b618c
Mahish <mahishs51@gmail.com>
2025-07-21 13:03:00 +0530

    Ranking Done

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from collections import defaultdict
-
-
-
-
 #Prints Welcome Text
-print("Welcome to SMR")
+print("Welcome to SMR V1")
 number_of_player_s= input("Enter the No of Players :")
 number_of_player = int(number_of_player_s)
 names = input("Enter the Names").split()
@@ -29,7 +24,35 @@ player_detail[3]['score'] += 40
 sorted_list = {k:v for k,v in sorted(player_detail.items(),key = lambda v:v[1]['score'] , reverse=True)}
 print(sorted_list)
 
-#paring players
+#paring players Higher Initial points paired together
 game1 = [sorted_list[3] , sorted_list[0]]
 game2 = [sorted_list[2] , sorted_list[1]]
+
+
+#check win or loss and print ranking
+ans1 = input("Who Won Game1 Press 1 for 3 and Press 2 for 0 ")
+if(ans1 == 1):
+    game1win = [sorted_list[3]]
+    sorted_list[3]['score'] += 10
+    sorted_list[0]['score'] -= 20
+else:
+    game1win = [sorted_list[0]]
+    sorted_list[0]['score'] += 10
+    sorted_list[3]['score'] -= 20
+ans2 = input("Who Won Game2 Press 1 for 2 and 2 for 1")
+if(ans2 == 1):
+    game2win = [sorted_list[2]]
+    sorted_list[2]['score'] += 10
+    sorted_list[1]['score'] -= 20
+
+else:
+    game2win = [sorted_list[1]]
+    sorted_list[1]['score'] += 10
+    sorted_list[2]['score'] -= 20
+
+update_sorted_list = {k:v for k,v in sorted(sorted_list.items(),key=lambda final:final[1]['score'] , reverse=True)}
+print("The final rank List is : ")
+print(update_sorted_list)
+
+
 
