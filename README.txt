29b99e110b12b82c72bf1333cafc2e6af1d464ff
Mahish <mahishs51@gmail.com>
2025-07-22 07:48:19 +0530

    Added Round Robin for 6-player only

--- README.txt
+++ README.txt
@@ -1,6 +1,7 @@
+Version 1: 
+
 Welcome to Swiss Manager Remake V1
 This is my first project which i made and didnt just vibe code(I did use google for sum stuff)
 1) It only has 4 members so yeah just input 4 for no of players
 2) It has only 1 round 
 3) It changes points and shows ranking in the form of descending form
- 
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #Prints Welcome Text
 print("Welcome to SMR V1")
+tournament_type = input("Do u Want Round Robin(6-10 players) Type 1 to select it")
 number_of_player_s= input("Enter the No of Players :")
 number_of_player = int(number_of_player_s)
+if type =='1' and number_of_player < 6:
+    print("Robin round wont work")
+    exit()
 names = input("Enter the Names").split()
 score = input("Enter the score").split()
 score_n = []
+
 for x in score:
     score_n.append(int(x))
 print(score_n)
@@ -21,13 +26,8 @@ for x in player_detail:
 
 
 #just test variables
-player_detail[0]['score'] += 0
-player_detail[1]['score'] += 0
-player_detail[2]['score'] += 0
-player_detail[3]['score'] += 0
-
-
-
+for x in player_detail:
+    player_detail[x]['score'] += 0
 
 
 #sorting thing(descending order)
@@ -35,31 +35,43 @@ sorted_list = {k:v for k,v in sorted(player_detail.items(),key = lambda v:v[1]['
 print(sorted_list)
 
 #paring players Higher Initial points paired together
-game1 = [sorted_list[3] , sorted_list[0]]
-game2 = [sorted_list[2] , sorted_list[1]]
+if number_of_player == 6:
+    game1 = [sorted_list[5],sorted_list[4]]
+    game2 = [sorted_list[3] , sorted_list[2]]
+    game3 = [sorted_list[1] , sorted_list[0]]
+    ans1 = input("Who Won Game-1 Press 1 for 5 and Press 2 for 4 ")
+    if(ans1 == '1'):
+        game1win = [sorted_list[5]]
+        sorted_list[5]['score'] += 10
+        sorted_list[4]['score'] -= 20
+    elif(ans1 == '2'):
+        game1win = [sorted_list[4]]
+        sorted_list[4]['score'] += 10
+        sorted_list[5]['score'] -= 20
+    ans2  = input("Who won game-2 press 1 for 3 and 2 for 2")
+    if(ans2 == '1'):
+        game2win = [sorted_list[3]]
+        sorted_list[3]['score'] += 10
+        sorted_list[2]['score'] -= 20
+    elif(ans2 == '2'):
+        game2win = [sorted_list[2]]
+        sorted_list[2]['score'] += 10
+        sorted_list[3]['score'] -= 20
+    ans3  = input("Who won game-3 press 1 for 1 and 2 for 0")
+    if(ans3 == '1'):
+        game3win = [sorted_list[1]]
+        sorted_list[1]['score'] += 10
+        sorted_list[0]['score'] -= 20
+    elif(ans3 == '2'):
+        game3win = [sorted_list[0]]
+        sorted_list[0]['score'] += 10
+        sorted_list[1]['score'] -= 20
+    
+
+
 
 
 #check win or loss and print ranking
-ans1 = input("Who Won Game1 Press 1 for 3 and Press 2 for 0 ")
-if(ans1 == 1):
-    game1win = [sorted_list[3]]
-    sorted_list[3]['score'] += 10
-    sorted_list[0]['score'] -= 20
-else:
-    game1win = [sorted_list[0]]
-    sorted_list[0]['score'] += 10
-    sorted_list[3]['score'] -= 20
-ans2 = input("Who Won Game2 Press 1 for 2 and 2 for 1")
-if(ans2 == 1):
-    game2win = [sorted_list[2]]
-    sorted_list[2]['score'] += 10
-    sorted_list[1]['score'] -= 20
-
-else:
-    game2win = [sorted_list[1]]
-    sorted_list[1]['score'] += 10
-    sorted_list[2]['score'] -= 20
-
 update_sorted_list = {k:v for k,v in sorted(sorted_list.items(),key=lambda final:final[1]['score'] , reverse=True)}
 print("The final rank List is : ")
 print(update_sorted_list)
